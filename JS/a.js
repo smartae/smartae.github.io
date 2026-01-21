2760d355cf483430aee3f9e3b71889163fcba3cd
smartae <smartae54@gmail.com>
2026-01-21 14:39:10 +0100

    Update a.js

--- JS/a.js
+++ JS/a.js
@@ -5,4 +5,22 @@ fetch("/get/t.json").then((d)=>d.json()).then((data)=>{
             <a href='/HG' onclick='localStorage.setItem("A","${v.a}");'>${v.n}</a>
         `;
     });
+
 });
+const dd=document.querySelectorAll("div.dd");
+let str="";
+for(let v of dd){
+    console.log(v);
+    v.addEventListener("click",()=>{
+        str+=v.getAttribute("d");
+        v.style.color="#c8c8c8";
+        if(str === "abc"){
+            window.open("https://smartae.github.io/6589/","_self");
+        }
+        setTimeout(()=>{
+            str="";
+            for(let k of dd)
+                k.style.color="white";
+        },3000);
+    });
+}
